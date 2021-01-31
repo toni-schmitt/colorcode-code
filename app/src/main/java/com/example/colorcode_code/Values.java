a48d164130846329f196325a7c83a68accb751f3
Toni Schmitt <tonischmitt@online.de>
2021-01-31 15:11:13 +0100

    fix crack bug

--- app/src/main/java/com/example/colorcode_code/Values.java
+++ app/src/main/java/com/example/colorcode_code/Values.java
@@ -6,38 +6,46 @@ import java.util.Hashtable;
 
 public class Values extends Application {
 
-    Hashtable<String, Double> dict_ring = new Hashtable<String, Double>() {
-        { put(getResources().getStringArray(R.array.rings)[0], 0.0); }
-        { put(getResources().getStringArray(R.array.rings)[1], 1.0); }
-        { put(getResources().getStringArray(R.array.rings)[2], 2.0); }
-        { put(getResources().getStringArray(R.array.rings)[3], 3.0); }
-        { put(getResources().getStringArray(R.array.rings)[4], 4.0); }
-        { put(getResources().getStringArray(R.array.rings)[5], 5.0); }
-        { put(getResources().getStringArray(R.array.rings)[6], 6.0); }
-        { put(getResources().getStringArray(R.array.rings)[7], 7.0); }
-        { put(getResources().getStringArray(R.array.rings)[8], 8.0); }
-        { put(getResources().getStringArray(R.array.rings)[9], 9.0); }
-        { put(getResources().getStringArray(R.array.rings)[10], 10.0); }
-    };
-    Hashtable<String, Double> dict_multi = new Hashtable<String, Double>(){
-        { put(getResources().getStringArray(R.array.multi)[0], 10.0); }
-        { put(getResources().getStringArray(R.array.multi)[1], 100.0); }
-        { put(getResources().getStringArray(R.array.multi)[2], 1000.0); }
-        { put(getResources().getStringArray(R.array.multi)[3], 10000.0); }
-        { put(getResources().getStringArray(R.array.multi)[4], 100000.0); }
-        { put(getResources().getStringArray(R.array.multi)[5], 1000000.0); }
-        { put(getResources().getStringArray(R.array.multi)[6], 10000000.0); }
-        { put(getResources().getStringArray(R.array.multi)[7], 100000000.0); }
-        { put(getResources().getStringArray(R.array.multi)[8], 0.1); }
-        { put(getResources().getStringArray(R.array.multi)[9], 0.01); }
-    };
-    Hashtable<String, Double> dict_tole = new Hashtable<String, Double>(){
-        { put(getResources().getStringArray(R.array.tole)[0], 0.1); }
-        { put(getResources().getStringArray(R.array.tole)[1], 0.2); }
-        { put(getResources().getStringArray(R.array.tole)[2], 0.05); }
-        { put(getResources().getStringArray(R.array.tole)[3], 0.025); }
-        { put(getResources().getStringArray(R.array.tole)[4], 0.01); }
-        { put(getResources().getStringArray(R.array.tole)[5], 0.5); }
-        { put(getResources().getStringArray(R.array.tole)[6], 0.10); }
-    };
+    Hashtable<String, Double> dict_ring;
+    Hashtable<String, Double> dict_multi;
+    Hashtable<String, Double> dict_tole;
+
+    public Values(){
+        dict_ring = new Hashtable<String, Double>() {
+            { put(getResources().getStringArray(R.array.rings)[0], 0.0); }
+            { put(getResources().getStringArray(R.array.rings)[1], 1.0); }
+            { put(getResources().getStringArray(R.array.rings)[2], 2.0); }
+            { put(getResources().getStringArray(R.array.rings)[3], 3.0); }
+            { put(getResources().getStringArray(R.array.rings)[4], 4.0); }
+            { put(getResources().getStringArray(R.array.rings)[5], 5.0); }
+            { put(getResources().getStringArray(R.array.rings)[6], 6.0); }
+            { put(getResources().getStringArray(R.array.rings)[7], 7.0); }
+            { put(getResources().getStringArray(R.array.rings)[8], 8.0); }
+            { put(getResources().getStringArray(R.array.rings)[9], 9.0); }
+            { put(getResources().getStringArray(R.array.rings)[10], 10.0); }
+        };
+
+        dict_multi = new Hashtable<String, Double>(){
+            { put(getResources().getStringArray(R.array.multi)[0], 10.0); }
+            { put(getResources().getStringArray(R.array.multi)[1], 100.0); }
+            { put(getResources().getStringArray(R.array.multi)[2], 1000.0); }
+            { put(getResources().getStringArray(R.array.multi)[3], 10000.0); }
+            { put(getResources().getStringArray(R.array.multi)[4], 100000.0); }
+            { put(getResources().getStringArray(R.array.multi)[5], 1000000.0); }
+            { put(getResources().getStringArray(R.array.multi)[6], 10000000.0); }
+            { put(getResources().getStringArray(R.array.multi)[7], 100000000.0); }
+            { put(getResources().getStringArray(R.array.multi)[8], 0.1); }
+            { put(getResources().getStringArray(R.array.multi)[9], 0.01); }
+        };
+
+        dict_tole = new Hashtable<String,Double>() {
+            { put(getResources().getStringArray(R.array.tole)[0], 0.1); }
+            { put(getResources().getStringArray(R.array.tole)[1], 0.2); }
+            { put(getResources().getStringArray(R.array.tole)[2], 0.05); }
+            { put(getResources().getStringArray(R.array.tole)[3], 0.025); }
+            { put(getResources().getStringArray(R.array.tole)[4], 0.01); }
+            { put(getResources().getStringArray(R.array.tole)[5], 0.5); }
+            { put(getResources().getStringArray(R.array.tole)[6], 0.10); }
+        };
+    }
 }
