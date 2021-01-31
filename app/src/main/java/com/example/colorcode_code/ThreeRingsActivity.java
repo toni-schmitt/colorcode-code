30439e24c679456a7c3e32fb764c0ead2a86b3ae
icekex1 <icekex23@gmail.com>
2021-01-31 15:02:36 +0100

    hashmaps good, with big crack bug sorry :(

--- app/src/main/java/com/example/colorcode_code/ThreeRingsActivity.java
+++ app/src/main/java/com/example/colorcode_code/ThreeRingsActivity.java
@@ -2,30 +2,43 @@ package com.example.colorcode_code;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class ThreeRingsActivity extends AppCompatActivity {
+import java.util.Hashtable;
 
+public class ThreeRingsActivity extends AppCompatActivity {
+    Spinner spinner_3_1;
+    Spinner spinner_3_2;
+    Spinner spinner_3_3;
+    Button btn;
+
+    Values values = ((Values) getApplicationContext());
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three_rings);
-        Spinner spinner_3_1 = findViewById(R.id.spinner_3_1);
-        Spinner spinner_3_2 = findViewById(R.id.spinner_3_2);
-        Spinner spinner_3_3 = findViewById(R.id.spinner_3_3);
+        btn = findViewById(R.id.btn);
+
+        spinner_3_1 = findViewById(R.id.spinner_3_1);
+        spinner_3_2 = findViewById(R.id.spinner_3_2);
+        spinner_3_3 = findViewById(R.id.spinner_3_3);
+
         ArrayAdapter<CharSequence> adapter_3_1 = ArrayAdapter.createFromResource(this, R.array.rings, android.R.layout.simple_spinner_item);
         adapter_3_1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_3_1.setAdapter(adapter_3_1);
         spinner_3_2.setAdapter(adapter_3_1);
+
         ArrayAdapter<CharSequence> adapter_3_2 = ArrayAdapter.createFromResource(this, R.array.multi, android.R.layout.simple_spinner_item);
         adapter_3_2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_3_3.setAdapter(adapter_3_2);
-
 
         findViewById(R.id.btn_Back_ThreeRings).setOnClickListener(v -> onBack());
     }
@@ -33,4 +46,9 @@ public class ThreeRingsActivity extends AppCompatActivity {
     public void onBack() {
         startActivity(new Intent(this, MainActivity.class));
     }
+
+    public void onClick(View view) {
+
+    }
 }
+
